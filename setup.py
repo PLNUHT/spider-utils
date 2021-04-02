4679343f48833e2149e5d12538bf1d05c7b3fc36
zengguoyang <qbjooo@qq.com>
2021-04-02 14:10:29 +0800

    include_package_data

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
     ],
     python_requires=">=3.6",
     install_requires=requirements,
+    include_package_data=True,
     package_data={
         "spider-utils": [
             "ualist.txt",
