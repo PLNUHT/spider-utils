eaaef490bc7832590b28a2e2b1fd45c03f8dff21
zengguoyang <qbjooo@qq.com>
2021-04-14 19:59:09 +0800

    新版本

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with  open("requirements.txt") as freq:
 
 setuptools.setup(
     name="spider-utils",
-    version="0.0.4",
+    version="0.0.5",
     author="a710128",
     author_email="qbjooo@qq.com",
     description="spider utils",
